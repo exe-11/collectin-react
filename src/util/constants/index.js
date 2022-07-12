df4b6eb61d8bdd8d365a023bfdbb8862d73ecb30
exe-11 <exe.sirius11@gmail.com>
2022-07-12 14:13:34 +0500

    fixed again

--- src/util/constants/index.js
+++ src/util/constants/index.js
@@ -4,7 +4,7 @@ const BACK_HOST = 'collectserver-env.eba-ujipkvzn.us-east-1.elasticbeanstalk.com
 const FRONT_HOST = '3.222.113.96:3000'
 // const BACK_HOST = '127.0.0.1:8080'
 // const FRONT_HOST = '127.0.0.1:3000'
-
+// Fixedg
 
 export const BASE_URL = 'http://' + BACK_HOST;
 
